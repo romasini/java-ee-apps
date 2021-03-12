f45a2c2044f4aac87fb55411a2b1395097a61ce2
Roman Sinitsyn <romasini@gmail.com>
2021-03-12 23:49:13 +0300

    Initial commit

---
+++ first-web-app/src/main/java/ru/romasini/FirstServlet.java
@@ -0,0 +1,43 @@
+package ru.romasini;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+
+//48
+public class FirstServlet implements Servlet {
+
+    private  static final Logger logger = LoggerFactory.getLogger(FirstServlet.class);
+    private ServletConfig servletConfig;
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        logger.info("Servlet init");
+        this.servletConfig = servletConfig;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return servletConfig;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        logger.info("New request");
+
+        servletResponse.getWriter().println("<h1>Hello JavaEE</h1>");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
